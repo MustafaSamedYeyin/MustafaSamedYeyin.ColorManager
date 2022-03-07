a50e67ac40d940340efa0b2d30cc66cd11fe5f19
Mustafa Samed Yeyin <mustafasamedyeyin@gmail.com>
2022-03-07 23:59:57 +0300

    Interface of repositories added for database operations.~

---
+++ Core/Interfaces/Data/Repository/IPalatteRepository.cs
@@ -0,0 +1,7 @@
+namespace Core.Interfaces.Data.Repository
+{
+    public interface IPalatteRepository
+    {
+
+    }
+}
---
+++ Core/Interfaces/Data/Repository/IUserRepository.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Core.Interfaces.Data.Repository
+{
+    public interface IUserRepository
+    {
+
+    }
+}
